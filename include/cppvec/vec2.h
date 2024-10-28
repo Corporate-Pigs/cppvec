4a26073542cbd59cecd687de52fd5ec7389c61cf
nflsilva <nelsonflsilva@gmail.com>
2024-10-28 00:00:31 +0000

    feat: add more vec2 methods

--- include/cppvec/vec2.h
+++ include/cppvec/vec2.h
@@ -3,11 +3,32 @@
 #include <cstdint>
 
 namespace cppvec {
+
 template <typename T>
 struct Vec2 {
     T x, y;
-    Vec2 operator+(const Vec2& other) const;
-    Vec2 operator-(const Vec2& other) const;
-    Vec2 operator*(const uint32_t scalar) const;
+    Vec2 operator+(const Vec2& other) const { return {this->x + other.x, y + other.y}; }
+    Vec2& operator+=(const Vec2& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Vec2 operator-(const Vec2& other) const { return {this->x - other.x, y - other.y}; }
+    Vec2& operator-=(const Vec2& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    template <typename S>
+    Vec2 operator*(const S scalar) const {
+        return {x * scalar, y * scalar};
+    }
+
+    template <typename S>
+    Vec2 operator/(const S scalar) const {
+        return {x / scalar, y / scalar};
+    }
 };
 }  // namespace cppvec
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,16 +1,1 @@
 #include "cppvec/vec2.h"
-
-template <typename T>
-cppvec::Vec2<T> cppvec::Vec2<T>::operator+(const cppvec::Vec2<T>& other) const {
-    return {this->x + other.x, y + other.y};
-}
-
-template <typename T>
-cppvec::Vec2<T> cppvec::Vec2<T>::operator-(const cppvec::Vec2<T>& other) const {
-    return {this->x - other.x, y - other.y};
-}
-
-template <typename T>
-cppvec::Vec2<T> cppvec::Vec2<T>::operator*(const uint32_t scalar) const {
-    return {this->x * scalar, y * scalar};
-}
