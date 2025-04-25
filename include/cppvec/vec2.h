e52caaade0be0a6c4d62ab5e9380590f7734d455
nflsilva <nelsonflsilva@gmail.com>
2025-04-25 22:16:53 +0100

    add default vec2 constructor with zeroed values

--- include/cppvec/vec2.h
+++ include/cppvec/vec2.h
@@ -7,6 +7,7 @@ namespace cppvec {
 template <typename T>
 struct Vec2 {
     Vec2(T x, T y) : x(x), y(y) {}
+    Vec2() : x(0), y(0) {}
     T x, y;
     Vec2 operator+(const Vec2& other) const { return {this->x + other.x, y + other.y}; }
     Vec2& operator+=(const Vec2& other) {
