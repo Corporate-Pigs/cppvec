0874a0ad076ab16e6477a7b95847778c27abe62f
nflsilva <nelsonflsilva@gmail.com>
2025-05-31 18:43:53 +0100

    add equals operator for vec2

--- include/cppvec/vec2.h
+++ include/cppvec/vec2.h
@@ -2,53 +2,64 @@
 
 #include <cstdint>
 
-namespace cppvec {
+namespace cppvec
+{
 
 template <typename T>
-struct Vec2 {
+struct Vec2
+{
     Vec2(T x, T y) : x(x), y(y) {}
     Vec2() : x(0), y(0) {}
     T x, y;
+    Vec2 operator==(const Vec2& other) const { return this->x == other.x && y == other.y; }
     Vec2 operator+(const Vec2& other) const { return {this->x + other.x, y + other.y}; }
-    Vec2& operator+=(const Vec2& other) {
+
+    Vec2& operator+=(const Vec2& other)
+    {
         x += other.x;
         y += other.y;
         return *this;
     }
 
     Vec2 operator-(const Vec2& other) const { return {this->x - other.x, y - other.y}; }
-    Vec2& operator-=(const Vec2& other) {
+    Vec2& operator-=(const Vec2& other)
+    {
         x -= other.x;
         y -= other.y;
         return *this;
     }
 
     Vec2 operator*(const Vec2& other) const { return {this->x * other.x, y * other.y}; }
-    Vec2& operator*=(const Vec2& other) {
+    Vec2& operator*=(const Vec2& other)
+    {
         x *= other.x;
         y *= other.y;
         return *this;
     }
 
     Vec2 operator/(const Vec2& other) const { return {this->x / other.x, y / other.y}; }
-    Vec2& operator/=(const Vec2& other) {
+    Vec2& operator/=(const Vec2& other)
+    {
         x /= other.x;
         y /= other.y;
         return *this;
     }
 
     template <typename S>
-    Vec2 operator+=(const S scalar) {
+    Vec2 operator+=(const S scalar)
+    {
         return {x + scalar, y + scalar};
     }
 
     template <typename S>
-    Vec2 operator*(const S scalar) const {
+    Vec2 operator*(const S scalar) const
+    {
         return {x * scalar, y * scalar};
     }
 
     template <typename S>
-    Vec2 operator/(const S scalar) const {
+    Vec2 operator/(const S scalar) const
+    {
         return {x / scalar, y / scalar};
     }
 };
